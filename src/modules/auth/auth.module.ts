a2439318012ef6d39ce994f4513917b623d6fa96
Raphael Kauan <raphaelkauanoficial@gmail.com>
2024-05-04 11:10:30 -0400

    add `login` generating JWT

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,9 +3,16 @@ import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { LocalStrategy } from "./strategies/local.stategy";
 import { UserModule } from "../user/user.module";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-    imports: [UserModule],
+    imports: [
+        UserModule,
+        JwtModule.register({
+            secret: process.env.JWT_SECRET,
+            signOptions: { expiresIn: "60d" },
+        }),
+    ],
     controllers: [AuthController],
     providers: [AuthService, LocalStrategy],
 })
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,10 +1,17 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { UserService } from "../user/user.service";
 import * as bcrypt from "bcrypt";
+import { User } from "../user/entities/user.entity";
+import { UserPayload } from "./models/UserPayload";
+import { JwtService } from "@nestjs/jwt";
+import { UserToken } from "./models/UserToken";
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly userService: UserService) {}
+    constructor(
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService,
+    ) {}
 
     async validateUser(email: string, password: string) {
         const user = await this.userService.findByEmail(email);
@@ -21,7 +28,19 @@ export class AuthService {
         throw new HttpException("email ou senha inválido!", HttpStatus.BAD_REQUEST);
     }
 
-    async login() {
-        return "login realizado com sucesso!";
+    login(user: User): UserToken {
+        // Transforma o user em um JWT
+
+        const payload: UserPayload = {
+            sub: user.id,
+            email: user.email,
+            name: user.name,
+        };
+
+        const jwtToken = this.jwtService.sign(payload);
+
+        return {
+            access_token: jwtToken,
+        };
     }
 }
