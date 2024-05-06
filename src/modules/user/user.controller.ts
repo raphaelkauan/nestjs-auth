b6e8e23957b1890bf4af4000b305b81a426ea2ef
Raphael Kauan <raphaelkauanoficial@gmail.com>
2024-05-06 18:23:19 -0400

    removing comments

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Post, Body } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { IsPublic } from "../auth/decorators/is-public.decorator";
-// import { UpdateUserDto } from "./dto/update-user.dto";
 
 @Controller("user")
 export class UserController {
@@ -13,24 +12,4 @@ export class UserController {
     create(@Body() createUserDto: CreateUserDto) {
         return this.userService.create(createUserDto);
     }
-
-    // @Get()
-    // findAll() {
-    //     return this.userService.findAll();
-    // }
-
-    // @Get(":id")
-    // findOne(@Param("id") id: string) {
-    //     return this.userService.findOne(+id);
-    // }
-
-    // @Patch(":id")
-    // update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
-    //     return this.userService.update(+id, updateUserDto);
-    // }
-
-    // @Delete(":id")
-    // remove(@Param("id") id: string) {
-    //     return this.userService.remove(+id);
-    // }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,6 +1,5 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
-// import { UpdateUserDto } from "./dto/update-user.dto";
 import { PrismaService } from "../../database/prisma.service";
 import * as bcrypt from "bcrypt";
 
@@ -45,20 +44,4 @@ export class UserService {
 
         return user;
     }
-
-    // findAll() {
-    //     return `This action returns all user`;
-    // }
-
-    // findOne(id: number) {
-    //     return `This action returns a #${id} user`;
-    // }
-
-    // update(id: number, updateUserDto: UpdateUserDto) {
-    //     return `This action updates a #${id} user`;
-    // }
-
-    // remove(id: number) {
-    //     return `This action removes a #${id} user`;
-    // }
 }
